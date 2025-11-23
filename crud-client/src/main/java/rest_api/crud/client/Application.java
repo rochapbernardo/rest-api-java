5c8f7cebb2d598cdc046f00c39276ed708a7fb88
rochapbernardo <bernardopolese2809@gmail.com>
2025-11-23 19:27:56 -0300

    update Application.java client

--- crud-client/src/main/java/rest_api/crud/client/Application.java
+++ crud-client/src/main/java/rest_api/crud/client/Application.java
@@ -1,13 +1,27 @@
 package rest_api.crud.client;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import rest_api.crud.client.terminal.UserClientCLI;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    private final UserClientCLI cli;
+
+    public Application(UserClientCLI cli) {
+        this.cli = cli;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runCLI() {
+        return args -> cli.start();
+    }
 
 }
