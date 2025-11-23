3dc1a65bf7bd60a04581898a1344239b5a450bbc
rochapbernardo <bernardopolese2809@gmail.com>
2025-11-23 19:29:19 -0300

    create User.java client

---
+++ crud-client/src/main/java/rest_api/crud/client/model/User.java
@@ -0,0 +1,60 @@
+package rest_api.crud.client.model;
+
+public class User {
+    private int id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String dt_nascimento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDt_nascimento() {
+        return dt_nascimento;
+    }
+
+    public void setDt_nascimento(String dt_nascimento) {
+        this.dt_nascimento = dt_nascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", dt_nascimento='" + dt_nascimento + '\'' +
+                '}';
+    }
+}
