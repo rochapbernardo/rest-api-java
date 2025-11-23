738a700e6e9dc92f037bc972bf7e51df61554e3d
rochapbernardo <bernardopolese2809@gmail.com>
2025-11-23 17:49:15 -0300

    initial commit client

---
+++ crud-client/src/test/java/rest_api/crud/client/ApplicationTests.java
@@ -0,0 +1,13 @@
+package rest_api.crud.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
