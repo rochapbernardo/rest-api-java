91bebc5f2984f10dc306894091109628b2eea68d
rochapbernardo <bernardopolese2809@gmail.com>
2025-11-23 16:02:29 -0300

    initial commit server

---
+++ crud-server/src/test/java/java_ws/rest/crud/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package java_ws.rest.crud.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
